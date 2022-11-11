9617b7605e6537d1b27a8816b316b33aca2894f3
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-11-11 14:48:38 +0200

    add modifier final to session's elements

--- src/main/java/com/bondarenko/onlineshop/security/Session.java
+++ src/main/java/com/bondarenko/onlineshop/security/Session.java
@@ -2,13 +2,12 @@ package com.bondarenko.onlineshop.security;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
+
 @AllArgsConstructor
 @Getter
-@Setter
 public class Session {
- private String token;
- private LocalDateTime expireDate;
+    private final String token;
+    private final LocalDateTime expireDate;
 }
