c3abefc0546c56f3141afa3f8b91f721c4e11e8d
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-11-14 12:46:03 +0200

    delete constructor from UserService

--- src/main/java/com/bondarenko/onlineshop/service/UserService.java
+++ src/main/java/com/bondarenko/onlineshop/service/UserService.java
@@ -2,15 +2,13 @@ package com.bondarenko.onlineshop.service;
 
 import com.bondarenko.onlineshop.dao.UserDao;
 import com.bondarenko.onlineshop.entity.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
     private final UserDao userDao;
-
-    public UserService(UserDao userDao) {
-        this.userDao = userDao;
-    }
 
     public User findUser(String login) {
         return userDao.findUser(login);
