c984ec3c9a0254df4ac54e64fbc8c3e36f63dbe1
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-11-15 16:32:30 +0200

    refactor AppConfiguration:
    - move dataSource to some class;
    - rename package to configuration;

---
+++ src/main/java/com/bondarenko/onlineshop/web/configuration/AppConfiguration.java
@@ -0,0 +1,14 @@
+package com.bondarenko.onlineshop.web.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource({"classpath:application.properties"})
+@ComponentScan(basePackages = {"com.bondarenko.onlineshop"}, excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.REGEX, pattern = "com.bondarenko.onlineshop.web")})
+public class AppConfiguration {
+
+}
