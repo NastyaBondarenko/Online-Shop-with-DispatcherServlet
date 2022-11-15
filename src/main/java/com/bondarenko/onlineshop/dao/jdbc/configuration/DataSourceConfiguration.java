7d4b654306a145f12388bb69921e33a04b43834d
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-11-15 16:28:29 +0200

    add DataSourceConfiguration:
    - add method getDataSource;
    - add annotation @Value with values of properties.

---
+++ src/main/java/com/bondarenko/onlineshop/dao/jdbc/configuration/DataSourceConfiguration.java
@@ -0,0 +1,31 @@
+package com.bondarenko.onlineshop.dao.jdbc.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource({"classpath:application.properties"})
+public class DataSourceConfiguration {
+    @Value("${db.password}")
+    private String password;
+
+    @Value("${db.username}")
+    private String user;
+
+    @Value("${db.url}")
+    private String url;
+
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url(url);
+        dataSourceBuilder.username(user);
+        dataSourceBuilder.password(password);
+        return dataSourceBuilder.build();
+    }
+}
