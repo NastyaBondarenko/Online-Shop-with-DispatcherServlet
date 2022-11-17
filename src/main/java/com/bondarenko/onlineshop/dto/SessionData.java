0e917a8d66b66dd3fe1b57ff684167788fb71538
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-11-17 23:14:49 +0200

    refactor SessionData:
    - add @AllArgsConstructor.

--- src/main/java/com/bondarenko/onlineshop/dto/SessionData.java
+++ src/main/java/com/bondarenko/onlineshop/dto/SessionData.java
@@ -1,11 +1,13 @@
 package com.bondarenko.onlineshop.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
 @RequiredArgsConstructor
 public class SessionData {
     private String token;
