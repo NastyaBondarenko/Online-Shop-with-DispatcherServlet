53ee29b41fb734f4c14336c4b72991929a179b41
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-11-19 01:21:25 +0200

    refactor UserService:
    - add Optional to findUser(String login);

--- src/main/java/com/bondarenko/onlineshop/service/UserService.java
+++ src/main/java/com/bondarenko/onlineshop/service/UserService.java
@@ -5,12 +5,14 @@ import com.bondarenko.onlineshop.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserDao userDao;
 
-    public User findUser(String login) {
-        return userDao.findUser(login);
+    public Optional<User> findUser(String login) {
+        return Optional.ofNullable(userDao.findUser(login));
     }
 }
