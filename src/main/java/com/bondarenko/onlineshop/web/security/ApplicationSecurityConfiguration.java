8a2ee225412e04881c6fb7bcba9ce5ec02045e4f
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-11-27 22:09:20 +0200

    add role and permission to security:
    - add admin and user roles;
    - add ApplicationUserPermission;
    - add ApplicationUserRole;

--- src/main/java/com/bondarenko/onlineshop/web/security/ApplicationSecurityConfiguration.java
+++ src/main/java/com/bondarenko/onlineshop/web/security/ApplicationSecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.bondarenko.onlineshop.web.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,9 +13,13 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.bondarenko.onlineshop.web.security.ApplicationUserPermission.USER_GET;
+import static com.bondarenko.onlineshop.web.security.ApplicationUserRole.ADMIN;
+import static com.bondarenko.onlineshop.web.security.ApplicationUserRole.USER;
+
 @Configuration
 @EnableWebSecurity
-public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {//WebSecurityConfig
+public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
 
@@ -26,9 +31,15 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "products", "product_cart")
-                .permitAll()
+                .antMatchers("/", "/products","/js/*").permitAll()
+//                .antMatchers("/products/cart/**").hasRole(USER.name())
+                .antMatchers("/products/**").hasRole(ADMIN.name())
+                .antMatchers(HttpMethod.POST,"/products/cart/{id}").hasAuthority(USER_GET.name())
+                .antMatchers(HttpMethod.DELETE,"/products/cart/delete").hasAuthority(USER_GET.name())
+                .antMatchers(HttpMethod.GET,"/products/cart").hasAuthority(USER_GET.name())
+                .antMatchers(HttpMethod.GET,"/products/cart/**").hasAnyRole(USER.name(),ADMIN.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -41,15 +52,15 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
         UserDetails user = User.builder()
                 .username("user")
                 .password(passwordEncoder.encode("pass"))
-                .roles("USER")
+                .roles(USER.name())
                 .build();
 
-//        UserDetails admin = User.builder()
-//                .username("admin")
-//                .password(passwordEncoder.encode("admin"))
-//                .roles("ADMIN")
-//                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles(ADMIN.name())
+                .build();
 
-        return new InMemoryUserDetailsManager(user);
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
