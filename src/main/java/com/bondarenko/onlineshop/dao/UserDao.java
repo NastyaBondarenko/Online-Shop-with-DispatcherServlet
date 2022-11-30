5dd1b826492dcc3450f17163b4e313aaf31ac179
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-11-30 18:07:44 +0200

    delete unused classes
    - ApplicationUserPermission;
    - UserDao.

--- src/main/java/com/bondarenko/onlineshop/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.bondarenko.onlineshop.dao;
-
-import com.bondarenko.onlineshop.entity.User;
-
-import java.util.Optional;
-
-public interface UserDao {
-    Optional<User> findUser(String login);
-}
